845faa27e47cdf3700e1d493ff5eba2db75474d0
yihui <yihui@shouyihuitekiMacBook-Pro.local>
2018-07-24 21:40:46 +0800

    change config.js

--- config.js
+++ config.js
@@ -3,6 +3,16 @@ module.exports = {
   'mongoUri': 'mongodb://localhost:27017/test'
 };
 
-devServer: {
-	disableHostCheck: true
-}
+const config = {
+    // .. rest of config object
+
+    // Server Configuration options
+    devServer: {
+        // .. rest of devserver options
+
+        host: '0.0.0.0',
+        disableHostCheck: true
+    },
+
+    // .. rest of config object
+};
